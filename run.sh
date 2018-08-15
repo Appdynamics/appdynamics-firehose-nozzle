32e1a3586b3deedf9a12053d1a99c90bf3093e3f
Pavan Krishna <pavan.krish123@gmail.com>
2018-08-15 13:17:36 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
-auth_method="uaa"
+echo "Doing UAA Auth"
 
-if [[ "${auth_method}" == "api" ]]; then ## ignored
-    echo "Doing API auth" ## ignored
-    export NOZZLE_API_URL=https://api.bosh-lite.com ## ignored
-    export NOZZLE_USERNAME=admin ## ignored
-    export NOZZLE_PASSWORD=admin ## ignored
-
-else ## CHANGE THE FOLLOWING 
-    echo "Doing UAA Auth"
-    export NOZZLE_UAA_URL=https://uaa.<domain>
-    export NOZZLE_TRAFFIC_CONTROLLER_URL=<doppler address>
-    export NOZZLE_USERNAME=<opentsb_firehose_username>
-    export NOZZLE_PASSWORD=<opentsb_firehose_password>
-fi
-
-export NOZZLE_SINK=STDOUT # one of the following STDOUT|MACHINEAGENT|CONTROLLER|SPLUNK etc...
+export NOZZLE_UAA_URL=<uaa address>   # cf curl /v2/info | jq .doppler_logging_endpoint
+export NOZZLE_TRAFFIC_CONTROLLER_URL=<doppler address>  # cf curl /v2/info | jq .token_endpoint
+export NOZZLE_USERNAME=<opentsb_firehose_username>
+export NOZZLE_PASSWORD=<opentsb_firehose_password>
+export NOZZLE_SINK=stdout # one of the following stdout|controller
 export NOZZLE_FIREHOSE_SUBSCRIPTION_ID=appdynamics.firehose
 export NOZZLE_SKIP_SSL=true
 export NOZZLE_SELECTED_EVENTS=ValueMetric,CounterEvent
+export APPD_SAMPLING_RATE: 2 
+export APPD_CONTROLLER_HOST: <controller>
+export APPD_ACCESS_KEY: <accesskey>
+export APPD_CONTROLLER_PORT: <port>
+export APPD_ACCOUNT: <account>
+
 
 go run main.go
