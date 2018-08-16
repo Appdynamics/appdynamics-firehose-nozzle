33f634a5511bfd1691971a7d46b176f75cbd6480
Pavan Krishna Nimmagadda <pavan.krishna@appdynamics.com>
2018-08-16 11:43:41 -0700

    Added Proper Filtering Logic`

--- sinks/controller_serializer.go
+++ sinks/controller_serializer.go
@@ -7,45 +7,74 @@ import (
 )
 
 type DataPoint struct {
-	Metric, Source string
+	Metric         string
 	Value          int64
+	Allowed       bool
 }
 
 type ControllerEventSerializer struct {
 	tier string
 }
 
+
 func NewControllerEventSerializer(tier_name string) *ControllerEventSerializer {
 	return &ControllerEventSerializer{tier: tier_name}
 }
 
 func (w *ControllerEventSerializer) BuildHttpStartStopEvent(event *events.Envelope) interface{} {
-	return genericSerializer(event, w.tier)
+	return nil
 }
 
 func (w *ControllerEventSerializer) BuildLogMessageEvent(event *events.Envelope) interface{} {
-	return genericSerializer(event, w.tier)
+	return nil
 }
 
 func (w *ControllerEventSerializer) BuildValueMetricEvent(event *events.Envelope) interface{} {
-	return genericSerializer(event, w.tier)
+	origin, name, deployment, index := event.GetOrigin(), event.GetValueMetric().GetName(), event.GetDeployment(), event.GetIndex()
+	alias, present := FilterMetrics(origin, name)
+	prefix := fmt.Sprintf("Server|Component:%v|Custom Metrics", w.tier)
+	if present {
+		return &DataPoint{
+			Metric: fmt.Sprintf("%v|%v|%v|%v|%v", prefix, alias, name, deployment, index),
+			Value:  int64(event.GetValueMetric().GetValue()),
+			Allowed: present}
+	} else {
+		return &DataPoint{Metric: "", Value:  int64(0), Allowed: present}
+	}
 }
 
 func (w *ControllerEventSerializer) BuildCounterEvent(event *events.Envelope) interface{} {
-	return genericSerializer(event, w.tier)
+	origin, name, deployment, index := event.GetOrigin(), event.GetCounterEvent().GetName(), event.GetDeployment(), event.GetIndex()
+	alias, present := FilterMetrics(origin, name)
+	prefix := fmt.Sprintf("Server|Component:%v|Custom Metrics", w.tier)
+	if present {
+		return &DataPoint{
+			Metric: fmt.Sprintf("%v|%v|%v|%v|%v", prefix, alias, name, deployment, index),
+			Value:  int64(event.GetCounterEvent().GetDelta()),
+			Allowed: present}
+	} else {
+		return &DataPoint{Metric: "", Value:  int64(0), Allowed: present }
+	}
 }
 
 func (w *ControllerEventSerializer) BuildErrorEvent(event *events.Envelope) interface{} {
-	return genericSerializer(event, w.tier)
+	return nil
 }
 
 func (w *ControllerEventSerializer) BuildContainerEvent(event *events.Envelope) interface{} {
-	return genericSerializer(event, w.tier)
+	return nil
 }
 
-func genericSerializer(event *events.Envelope, tier string) *DataPoint {
-	return &DataPoint{
-		Metric: fmt.Sprintf("Server|Component:%v|Custom Metrics|%v|%v|%v", tier, event.GetOrigin(), event.GetDeployment(), event.GetIndex()),
-		Value:  int64(event.GetValueMetric().GetValue()),
-		Source: event.GetOrigin()}
+
+func FilterMetrics(eventOrigin, eventName string)(string, bool){
+	filters, present := MetricFilter[eventOrigin]
+	if present {
+		alias := MetricAlias[eventOrigin]
+		for _, allowedMetric := range filters {
+			if allowedMetric == eventName {
+				return alias, true
+			}
+		}
+	} 
+	return "", false
 }
--- sinks/controller_writer.go
+++ sinks/controller_writer.go
@@ -5,7 +5,7 @@ import (
 	"log"
 )
 
-type ControllerClient struct{}
+type ControllerClient struct{logger *log.Logger}
 
 func NewControllerClient(host, accessKey, account, app, tier, node string, port uint16, useSSL bool, logger *log.Logger) *ControllerClient {
 	cfg := appd.Config{}
@@ -21,7 +21,7 @@ func NewControllerClient(host, accessKey, account, app, tier, node string, port 
 	cfg.InitTimeoutMs = 1000
 	appd.InitSDK(&cfg)
 	logger.Println(&cfg.Controller)
-	return &ControllerClient{}
+	return &ControllerClient{logger: logger}
 }
 
 func (c *ControllerClient) PostBatch(events []interface{}) error {
@@ -32,13 +32,14 @@ func (c *ControllerClient) PostBatch(events []interface{}) error {
 			if !ok {
 				continue
 			}
-			if dataPoint.Source == "gorouter" || dataPoint.Source == "uaa" {
+		
+			if dataPoint.Allowed {
 				appd.AddCustomMetric("", dataPoint.Metric, appd.APPD_TIMEROLLUP_TYPE_AVERAGE,
 					appd.APPD_CLUSTERROLLUP_TYPE_INDIVIDUAL, appd.APPD_HOLEHANDLING_TYPE_REGULAR_COUNTER)
-
+				
+				c.logger.Printf("Posting %v with value %v", dataPoint.Metric, dataPoint.Value)
 				appd.ReportCustomMetrics("", dataPoint.Metric, dataPoint.Value)
 			}
-
 		}
 	}
 	appd.EndBT(bt)
